931ffd2e2efee8f391edbed4e5341dd3d282d2df
noyunseo <s2542@e-mirim.hs.kr>
2026-04-26 14:03:20 +0900

    [level 0] Title: 수 조작하기 1, Time: 5.70 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n, String control) {
+        for(int i=0; i<control.length(); i++){
+            char a=control.charAt(i);
+            if(a=='w'){
+                n+=1;
+            }else if(a=='s'){
+                n-=1;
+            }else if(a=='d'){
+                n+=10;
+            }else{
+                n-=10;
+            }
+        }
+        return n;
+    }
+}
